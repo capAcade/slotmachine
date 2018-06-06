20dd2b307a8d76df797071fc2bd1721e301fd8b1
Hans Blum <hans.blum@nl.abnamro.com>
2018-06-06 22:25:09 +0200

    Cleaner separation of concerns by using the SlotMachineController.

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # slotmachine
+
 A slot machine for the arcade
+
+##What
+
+A very simple fruit (or slot) machine. Press <space> to spin, click on insert coin to get 4 more credits.
+
+### Credits
+
+- You get two credits when you have two cherries on the center row.
+- If you have three of the same symbols next to each other on the center row, you credits increase two times the value of that symbol
+- if you all four reels have the same value on the center row, you increase with six times the value of that symbol 
+ 
+### Symbol values
+
+ - cherry: 1
+ - bar: 2
+ - plum: 4
+ - seven: 7
+ - watermelon: 11
+ - bell: 16
+ 
+ ## Why
+ 
+ It didn't seem that hard to build a slot machine and it can look nice, when it is finished.
+ 
+ ## How
+ 
+ You need to have stable `nodejs` installed.
+ clone the repository, run `npm install` and then `npm start`
+
+## Todo
+
+- Use nice (svg) images for the symbols.
+- Animate the spinning
+- add cool and annoying sounds
+- add fake payment options
+
+## Issues
+
+If you find issues, please report bugs on https://github.com/capAcade/slotmachine/issues]
+
--- slotmachine.js
+++ slotmachine.js
@@ -5,10 +5,69 @@
  * that faceTypes credits.
  */
 
+class SlotMachineController {
+    constructor() {
+        this.slotMachine = new SlotMachine(new FaceTypes())
+        this.creditsElement = document.getElementById('credits')
+        this.reelElements = [[
+                document.getElementById('reel0-top'),
+                document.getElementById('reel1-top'),
+                document.getElementById('reel2-top'),
+                document.getElementById('reel3-top')
+            ],[
+                document.getElementById('reel0'),
+                document.getElementById('reel1'),
+                document.getElementById('reel2'),
+                document.getElementById('reel3')
+            ],
+            [
+                document.getElementById('reel0-bottom'),
+                document.getElementById('reel1-bottom'),
+                document.getElementById('reel2-bottom'),
+                document.getElementById('reel3-bottom')
+            ]
+        ]
+        this.slotMachine.addEventListener('creditschanged', (credits) => {
+            this.creditsElement.innerHTML = '' + credits
+        })
+
+        this.slotMachine.addEventListener('reelchanged', (reel) => {
+            for(let i=0; i<this.reelElements.length;i++) {
+                this.reelElements[i][reel.position].innerHTML = reel.getFace(i-1).name
+            }
+        })
+
+        document.querySelector("input[value='Insert Coin'][type='button']").onclick = () => {this.slotMachine.addCredits(4)}
+    }
+
+    play() {
+        this.addKeyPressEventListener()
+        this.slotMachine.play()
+    }
+
+    addKeyPressEventListener() {
+        window.addEventListener('keypress', (keyEvent) => {
+            if (this.slotMachine.state !== 'playing') {
+            return
+            }
+            if (keyEvent.key === ' ' || keyEvent.key === 'Space bar') {
+                this.slotMachine.spin().then(this.slotMachine.recalculateCredits, (error) => console.log(error))
+                return
+            }
+            if (keyEvent.key === 'q' || keyEvent.key === 'Q') {
+                this.quit()
+            }
+        })
+    }
+
+    quit() {
+        this.slotMachine.quit()
+    }
+}
 
 class SlotMachine {
     constructor(FACE_TYPES) {
-        this.credits = 20
+        this.credits = 0
         this.reels = [
             new Reel(this, 0, FACE_TYPES.all), 
             new Reel(this, 1, FACE_TYPES.all),
@@ -16,84 +75,104 @@ class SlotMachine {
             new Reel(this, 3, FACE_TYPES.all)
         ]
         this.state = undefined
+        this.eventListeners = {
+            'nothing': []
+        }
+        this.recalculateCredits = this._recalculateCredits.bind(this)
+        this.score = this._score.bind(this)
+        this.twoCherries = this._twoCherries.bind(this)
+        this.allReelsHaveSameFace = this._allReelsHaveSameFace.bind(this)
+        this.threeConnectedReelsHaveSameFace = this._threeConnectedReelsHaveSameFace.bind(this)
     }
 
     play() {
-        this.refresh()
-        window.addEventListener("keypress", (keyEvent) => {
-            if (this.state !== 'playing') {
-                return
-            }
-            if (keyEvent.key === ' ' || keyEvent.key === 'Space bar') {
-                this.spin()
-                return
-            }
-            if (keyEvent.key === 'q' || keyEvent.key === 'Q') {
-                this.quit()
-            }
-        })
         this.state = 'playing';
+        this.addCredits(20)
+    }
+
+    addEventListener(name, listeningFunction) {
+        if (!this.eventListeners[name])  {
+            this.eventListeners[name] = []
+        }
+        this.eventListeners[name].push(listeningFunction)
+    }
+
+    publish(eventName, data) {
+        if (!this.eventListeners) {
+            return
+        }
+        for (let listener of this.eventListeners[eventName]) {
+            if (typeof(listener) === typeof(function(){})) {
+                listener(data)
+            }
+        }
+    }
+
+    addCredits(n) {
+        this.credits += n
+        this.publish('creditschanged', this.credits)
     }
 
     spin() {
-        for (let reel of this.reels) {
-            reel.spin() 
-        }
-        this.credits--
-        this.score()
-        this.refresh()
-        if (this.credits <= 0)  {
-            this.quit()
-        }
+        return new Promise((resolve, reject) => {
+            if (this.credits > 0) {
+                for (let reel of this.reels) {
+                    reel.spin()
+                }
+                resolve()
+            } else {
+                reject('no more credits')
+            }
+        })
     }
 
-    score() {
+
+    _recalculateCredits() {
+        return new Promise((resolve) => {
+            this.addCredits(-1)
+            this.score()
+        })
+    }
+
+    _score() {
         if (this.allReelsHaveSameFace()) {
-            this.credits += (6 * this.reels[3].getFace().value)
+            this.addCredits(6 * this.reels[3].getFace(0).value)
             return
         }
         if (this.threeConnectedReelsHaveSameFace()) {
-            this.credits += (2 * this.reels[2].getFace().value)
+            this.addCredits(2 * this.reels[2].getFace(0).value)
             return
         }
         if (this.twoCherries()) {
-            this.credits += 3
+            this.addCredits(3)
             return
         }
     }
 
-    threeConnectedReelsHaveSameFace() {
+    _threeConnectedReelsHaveSameFace() {
         if (this.reels[1].isSame(this.reels[2])) {
             return this.reels[0].isSame(this.reels[1]) || this.reels[3].isSame(this.reels[1])
         }        
         return false
     }
 
-    allReelsHaveSameFace()  {
+    _allReelsHaveSameFace()  {
         return
             this.reels[3].isSame(this.reels[0]) && 
             this.reels[2].isSame(this.reels[0]) && 
             this.reels[1].isSame(this.reels[0])          
     }
 
-    twoCherries() {
-        if (this.reels[1].isSame(this.reels[0]) || this.reels[1].isSame(this.reels[2])) {
-            return this.reels[1].getFace().name === 'cherry'
+    _twoCherries() {
+        if (this.reels[1].isSame(this.reels[0]) || this.reels[1].isSame(this.reels[2]) ) {
+            return this.reels[1].getFace(0).name === 'cherry'
         }
         if (this.reels[2].isSame(this.reels[3])) {
-            return this.reels[2].getFace().name === 'cherry'
+            return this.reels[2].getFace(0).name === 'cherry'
         }
-    }
-
-    refresh() {
-        for (let reel of this.reels) {
-            document.getElementById(reel.id).innerHTML = reel.getFace().name
-        }
-        document.getElementById('credits').innerHTML = this.credits; 
     }
 
     quit() {
-        console.log('Insert coins')
         this.state = 'quit'
     }
 }
@@ -110,15 +189,23 @@ class Reel {
 
     spin() {
         this.currentFaceIndex = (this.currentFaceIndex + Math.floor(Math.random() * this.faces.length)) % this.faces.length;
+        this.slotMachine.publish('reelchanged', this)
     }
 
-    getFace() {
-        return this.faces[this.currentFaceIndex]
+    getFace(n) {
+        let i = (this.currentFaceIndex + n) % this.faces.length
+        if (i<0) {
+            i = this.faces.length + i
+        }
+        return this.faces[i]
     }
+
 
     isSame(other) {
         return this.faces[this.currentFaceIndex] === other.faces[other.currentFaceIndex] 
     }
+
+
  
 }
 
@@ -133,4 +220,7 @@ class FaceTypes {
         this.all = [this.cherry, this.bar, this.plum, this.seven, this.watermelon, this.bell]
     }
 }
-new SlotMachine(new FaceTypes()).play()
+
+window.onload = function() {
+    new SlotMachineController().play()
+}
